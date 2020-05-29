5f9c1539837fe97d4cb7c381baaa2993d4f66f3c
Duvan <duvan4448@gmail.com>
2020-05-28 23:29:05 -0500

    2-Usuarios-2

--- AspNetMVCIdentity/AspNetMVCIdentity/Controllers/UserAdminController.cs
+++ AspNetMVCIdentity/AspNetMVCIdentity/Controllers/UserAdminController.cs
@@ -90,7 +90,7 @@ namespace IdentitySample.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = userViewModel.Email, Email = userViewModel.Email };
+                var user = new ApplicationUser { UserName = userViewModel.Email, Email = userViewModel.Email,Nombre=userViewModel.Nombre,Apellidos=userViewModel.Apellidos,Ciudad=userViewModel.Ciudad,FechaNacimiento=userViewModel.FechaNacimiento };
                 var adminresult = await UserManager.CreateAsync(user, userViewModel.Password);
 
                 //Add User to the selected Roles 
@@ -141,6 +141,10 @@ namespace IdentitySample.Controllers
             {
                 Id = user.Id,
                 Email = user.Email,
+                Nombre = user.Nombre,
+                Apellidos = user.Apellidos,
+                Ciudad = user.Ciudad,
+                FechaNacimiento = user.FechaNacimiento,
                 RolesList = RoleManager.Roles.ToList().Select(x => new SelectListItem()
                 {
                     Selected = userRoles.Contains(x.Name),
@@ -154,7 +158,7 @@ namespace IdentitySample.Controllers
         // POST: /Users/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "Email,Id")] EditUserViewModel editUser, params string[] selectedRole)
+        public async Task<ActionResult> Edit([Bind(Include = "Email,Id,Nombre,Apellidos, Ciudad, FechaNacimiento")] EditUserViewModel editUser, params string[] selectedRole)
         {
             if (ModelState.IsValid)
             {
@@ -166,6 +170,10 @@ namespace IdentitySample.Controllers
 
                 user.UserName = editUser.Email;
                 user.Email = editUser.Email;
+                user.Nombre = editUser.Nombre;
+                user.Apellidos = editUser.Apellidos;
+                user.Ciudad = editUser.Ciudad;
+                user.FechaNacimiento = editUser.FechaNacimiento;
 
                 var userRoles = await UserManager.GetRolesAsync(user.Id);
 
